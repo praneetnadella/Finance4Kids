0733afc32b04a64a67abbb6d6f5275eb51b5b1ed
praneetnadella <praneetnadella@gmail.com>
2020-02-16 15:00:45 -0500

    Final Game Changes

--- lib/game.dart
+++ lib/game.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 import 'dart:math';
 
+  //Public variables
   int _balance = 100;
   int _owned = 0;
+  int _price = 0;
+  int _shares = 0;
+  double shares = 0.0;
 
 class StockGame extends StatefulWidget {
   @override
@@ -13,25 +17,31 @@ class StockGame extends StatefulWidget {
 }
 
 class _StockGameState extends State<StockGame> {
-  int _price = 0;
-  int _shares = 0;
-  double shares = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
         title: Text("Stock Market Game"),
       ),
-      body: Container (child: 
-        Column (children: <Widget>[
+      body: Column (children: <Widget>[
+
+          //First Text Outputs
+          SizedBox(height: 70),
           Text("PRICE: \$" + _price.toString(),
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30, fontFamily: 'Oswald'),),
+          SizedBox(height: 35),
           Text("OWNED: " + _owned.toString(),
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30, fontFamily: 'Oswald'),),
+          SizedBox(height: 35),
           Text("SHARES: " + _shares.toString(),
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30, fontFamily: 'Oswald'),),
-          Slider.adaptive (
+          SizedBox(height: 35),
+          
+          //Slider
+          Container (
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Slider.adaptive (
             max: 20.0, min: 0.0, activeColor: Colors.orange, value: shares,
             onChanged: (double newValue) {
               setState(() {
@@ -39,7 +49,13 @@ class _StockGameState extends State<StockGame> {
                 shares = newValue;
               });
             },),
-          Row (children: <Widget>[
+          ),
+          SizedBox(height: 20),
+          
+          //Buttons
+          Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
             RaisedButton(
               child: Text("Sell", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20, fontFamily: 'Oswald'),), 
               onPressed: () => {
@@ -64,12 +80,15 @@ class _StockGameState extends State<StockGame> {
                 }
               },
               color: Colors.orange,),
+
+              SizedBox(width: 25),
               RaisedButton(
               child: Text("Hold", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20, fontFamily: 'Oswald'),), 
               onPressed: () => {
                 random(),
               },
               color: Colors.orange,),
+              SizedBox(width: 25),
               RaisedButton(
               child: Text("Buy", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20, fontFamily: 'Oswald'),), 
               onPressed: () => {
@@ -95,11 +114,13 @@ class _StockGameState extends State<StockGame> {
               },
               color: Colors.orange,),
           ],),
+          
+          //
+          SizedBox(height: 50),
           Text("BALANCE: \$" + _balance.toString(),
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 35, fontFamily: 'Oswald'),),
         ],),
          // This trailing comma makes auto-formatting nicer for build methods.
-      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.pop(context);
@@ -110,25 +131,26 @@ class _StockGameState extends State<StockGame> {
       );
   }
 
+  //Sets random price
   void random() {
     var rng = new Random();
     setState(() {
       int rand = rng.nextInt(10);
-      if (rand <= 10) {
-        _price = rng.nextInt(750000000);
+      if (rand <= 7) {
+        _price = rng.nextInt(75);
       }
       else {
-        _price = rng.nextInt(1000);
+        _price = rng.nextInt(100);
       }
     });
   }
 
-  void check() {
-    if (_balance >= 1000) {
+  // void check() {
+  //   if (_balance >= 1000) {
 
-    }
-    else if (_balance >= 1000000) {
+  //   }
+  //   else if (_balance >= 1000000) {
       
-    }
-  }
+  //   }
+  // }
 }
